ebd084a1529e672d5894298aea0d0fe683fbde61
fanusaez <fanusaez1@gmail.com>
2024-11-30 18:37:12 -0300

    Agrego logs de info para ver los pings y cosas que nos interesen mostrar en caso de que nos pidan.
    Para que se muestren estos logs se debe correr las terminales como
    
    RUST_LOG=info cargo run <puerto> <posx> <posy>

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{Read};
+use env_logger;
+use log::{debug, info, warn, error};
 
 use crate::driver::Driver;
 use crate::utils::read_config_file;
@@ -15,6 +17,7 @@ const CONFIG_PATH: &str = "config.txt";
 /// Receives a port and creates driver actor
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
     let args: Vec<String> = std::env::args().collect();
     let port = args[1].parse::<u16>().unwrap();
     let initial_position : (i32, i32) = (args[2].parse::<i32>().unwrap(), args[3].parse::<i32>().unwrap());
